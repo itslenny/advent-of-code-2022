e9d506eb8ef625f9c75f5e83c9366c762b886e94
itslenny <lenny@itslennysfault.com>
2022-12-03 14:32:51 -0600

    Day 2

--- src/tasks/day-02-1.js
+++ src/tasks/day-02-1.js
@@ -1,7 +1,36 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 1', inputLines.length);
+function getScore(p1, p2) {
 
-    inputLines.forEach(line => {
+    const choiceScores = {
+        X: 1, // rock
+        Y: 2, // paper
+        Z: 3, // scissors
+    };
 
-    });    
+    const p1ChoiceMap = {
+        A: 'X', // rock
+        B: 'Y', // paper
+        C: 'Z', // scissors
+    }
+
+    let winScore = 0;
+    if (p1ChoiceMap[p1] === p2) {
+        // draw
+        winScore = 3;
+    } else if (
+        (p2 === 'X' && p1 === 'C') ||
+        (p2 === 'Y' && p1 === 'A') ||
+        (p2 === 'Z' && p1 === 'B')
+    ) {
+        // win
+        winScore = 6;
+    }
+
+    return winScore + choiceScores[p2];
+}
+
+export default function calculateTotalScore(inputLines) {
+    return inputLines.reduce((acc, line) => {
+        const vals = line.split(' ');
+        return acc + getScore(vals[0], vals[1]);
+    }, 0);
 }
--- src/tasks/day-02-2.js
+++ src/tasks/day-02-2.js
@@ -1,7 +1,53 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 2', inputLines.length);
+function getScore(p1, p2) {
 
-    inputLines.forEach(line => {
+    const choiceScores = {
+        X: 1, // rock
+        Y: 2, // paper
+        Z: 3, // scissors
+    };
 
-    });
+    const p1ChoiceMap = {
+        A: 'X', // rock
+        B: 'Y', // paper
+        C: 'Z', // scissors
+    }
+
+    let winScore = 0;
+
+    // father forgive more for I have sinned
+    // this answer is so dumb, but if it works it's not dumb. I know I could've been more clever here
+    
+    if (p2 === 'X') { // lose
+        if (p1 === 'A') {
+            return choiceScores['Z'];
+        } else if (p1 === 'B') {
+            return choiceScores['X'];
+        } else {
+            return choiceScores['Y'];
+        }
+    } else if (p2 === 'Y') { // draw
+        winScore = 3;
+        if (p1 === 'A') {
+            return choiceScores['X'] + 3;
+        } else if (p1 === 'B') {
+            return choiceScores['Y'] + 3;
+        } else {
+            return choiceScores['Z'] + 3;
+        }
+    } else { // Z = win
+        if (p1 === 'A') {
+            return choiceScores['Y'] + 6;
+        } else if (p1 === 'B') {
+            return choiceScores['Z'] + 6;
+        } else {
+            return choiceScores['X'] + 6;
+        }
+    }
+}
+
+export default function calculateTotalScore(inputLines) {
+    return inputLines.reduce((acc, line) => {
+        const vals = line.split(' ');
+        return acc + getScore(vals[0], vals[1]);
+    }, 0);
 }
