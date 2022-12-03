bd4e0a662c25afadb0b2d29468d711aec93b2daa
itslenny <lenny@itslennysfault.com>
2022-12-03 14:33:07 -0600

    Day 3

--- src/tasks/day-03-1.js
+++ src/tasks/day-03-1.js
@@ -1,7 +1,29 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 1', inputLines.length);
-
+export default function countPriorityOfMisplacedItems(inputLines) {
+    const dupes = [];
     inputLines.forEach(line => {
+        const items = new Set();
+        const chars = line.split('');
 
-    });    
+        for (let i = 0; i < chars.length; i++) {
+            const c = chars[i];
+
+            if (i < line.length / 2) {
+                items.add(c);
+            } else {
+                if (items.has(c)) {
+                    dupes.push(c);
+                    // one dupe per sack, we're done
+                    break;
+                }
+            }
+        }
+
+    });
+
+    return dupes.reduce((acc, v) => {
+        const charCode = v.charCodeAt(0);
+        // a = 97, z = 122, A = 65, Z = 90
+        const charVal = charCode >= 97 ? charCode - 96 : charCode - 38;
+        return acc + charVal;
+    }, 0)
 }
--- src/tasks/day-03-2.js
+++ src/tasks/day-03-2.js
@@ -1,7 +1,28 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 2', inputLines.length);
+export default function countPriorityOfBadges(inputLines) {
+    const badges = [];
 
-    inputLines.forEach(line => {
+    const items = [new Set(), new Set()];
 
+    inputLines.forEach((line, i) => {
+        if (i % 3 === 2) {
+            const chars = line.split('');
+            for (let i = 0; i < chars.length; i++) {
+                const c = chars[i];
+                if (items[0].has(c) && items[1].has(c)) {
+                    badges.push(c);
+                    // we found the item, exit
+                    break;
+                }
+            }
+        } else {
+            items[i % 3] = new Set(line.split(''));
+        }
     });
+
+    return badges.reduce((acc, v) => {
+        const charCode = v.charCodeAt(0);
+        // a = 97, z = 122, A = 65, Z = 90
+        const charVal = charCode >= 97 ? charCode - 96 : charCode - 38;
+        return acc + charVal;
+    }, 0)
 }
