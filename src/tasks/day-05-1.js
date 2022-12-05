76119b5af50daf56360b0fb014312c36ced75398
itslenny <lenny@itslennysfault.com>
2022-12-05 12:56:47 -0600

    Day 5

--- src/tasks/day-05-1.js
+++ src/tasks/day-05-1.js
@@ -1,7 +1,30 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 1', inputLines.length);
+export default function arrangeCrates(inputLines) {
+    const breakIdx = inputLines.indexOf('');
+    const crateData = inputLines.slice(0, breakIdx);
+    const moveData = inputLines.slice(breakIdx + 1);
 
-    inputLines.forEach(line => {
+    const crates = [];
 
-    });    
+    crateData[crateData.length - 1].split('').forEach((crate, offset) => {
+        if (crate !== ' ') {
+            const crateContents = [];
+            for (let i = crateData.length - 2; i >= 0; i--) {
+                const char = crateData[i].charAt(offset);
+                if (char === ' ') {
+                    break;
+                }
+                crateContents.push(char);
+            }
+            crates.push(crateContents);
+        }
+    });
+
+    moveData.forEach(move => {
+        const [count, from, to] = move.match(/\d+/g);
+        for (let i = 0; i < count; i++) {
+            crates[to - 1].push(crates[from - 1].pop());
+        }
+    });
+
+    return crates.reduce((acc, v) => acc + (v.length > 0 ? v[v.length - 1] : ' '), '');
 }
--- src/tasks/day-05-2.js
+++ src/tasks/day-05-2.js
@@ -1,7 +1,29 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 2', inputLines.length);
+export default function arrangeCrates(inputLines) {
+    const breakIdx = inputLines.indexOf('');
+    const crateData = inputLines.slice(0, breakIdx);
+    const moveData = inputLines.slice(breakIdx + 1);
 
-    inputLines.forEach(line => {
+    const crates = [];
 
+    crateData[crateData.length - 1].split('').forEach((crate, offset) => {
+        if (crate !== ' ') {
+            const crateContents = [];
+            for (let i = crateData.length - 2; i >= 0; i--) {
+                const char = crateData[i].charAt(offset);
+                if (char === ' ') {
+                    break;
+                }
+                crateContents.push(char);
+            }
+            crates.push(crateContents);
+        }
     });
+
+    moveData.forEach(move => {
+        const [count, from, to] = move.match(/\d+/g);
+        const fromCrate = crates[from - 1];
+        crates[to - 1].push(...fromCrate.splice(fromCrate.length - count, count));
+    });
+
+    return crates.reduce((acc, v) => acc + (v.length > 0 ? v[v.length - 1] : ' '), '');
 }
