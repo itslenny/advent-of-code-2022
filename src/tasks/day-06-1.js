d3f2430ece9602ea304e51847b1422e4e2f7fca3
itslenny <lenny@itslennysfault.com>
2022-12-07 17:35:27 -0600

    Day 6

--- src/tasks/day-06-1.js
+++ src/tasks/day-06-1.js
@@ -1,7 +1,12 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 1', inputLines.length);
+export default function getStartOfPacketMarkers(inputLines) {
+    const uniqueStrLength = 4;
+    return inputLines.map(line => {
 
-    inputLines.forEach(line => {
-
+        for (let i = 0; i < line.length - uniqueStrLength; i++) {
+            const charSet = new Set(line.substr(i, uniqueStrLength).split(''));
+            if (charSet.size === uniqueStrLength) {
+                return i + uniqueStrLength;
+            }
+        }
     });    
 }
--- src/tasks/day-06-2.js
+++ src/tasks/day-06-2.js
@@ -1,7 +1,12 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 2', inputLines.length);
+export default function getStartOfMessageMarkers(inputLines) {
+    const uniqueStrLength = 14;
+    return inputLines.map(line => {
 
-    inputLines.forEach(line => {
-
-    });
+        for (let i = 0; i < line.length - uniqueStrLength; i++) {
+            const charSet = new Set(line.substr(i, uniqueStrLength).split(''));
+            if (charSet.size === uniqueStrLength) {
+                return i + uniqueStrLength;
+            }
+        }
+    });    
 }
