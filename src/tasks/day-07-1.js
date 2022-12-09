b718c2e232cbf36cc329bddd49ee8f6ea6770047
itslenny <lenny@itslennysfault.com>
2022-12-09 11:29:01 -0600

    Day 7

--- src/tasks/day-07-1.js
+++ src/tasks/day-07-1.js
@@ -1,7 +1,44 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 1', inputLines.length);
+export default function computeFileSizesToDelete(inputLines) {
+    let path = ['root'];
+    let dirSizes = {};
+    let isList = false;
 
     inputLines.forEach(line => {
 
-    });    
+        if (line.startsWith('$')) {
+            // it is a command
+            const parts = line.split(' ');
+            const cmd = parts[1];
+
+            if (cmd === 'ls') {
+                isList = true;
+            } else if (cmd === 'cd') {
+                isList = false;
+
+                const param = parts[2];
+                if (param === '/') {
+                    path = ['root'];
+                } else if (param === '..') {
+                    path.pop();
+                } else {
+                    path.push(param);
+                }
+            }
+        } else if (isList) {
+            // output
+            const [size, name] = line.split(' ');
+
+            if (size !== 'dir') {
+                for (let i = 0; i <= path.length; i++) {
+                    const curPath = path.slice(0, i).join('/');
+                    if (!(curPath in dirSizes)) {
+                        dirSizes[curPath] = 0;
+                    }
+                    dirSizes[curPath] += parseInt(size, 10);
+                }
+            }
+        }
+    });
+
+    return Object.values(dirSizes).filter(s => s <= 100000).reduce((acc, v) => acc + v, 0);
 }
--- src/tasks/day-07-2.js
+++ src/tasks/day-07-2.js
@@ -1,7 +1,48 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 2', inputLines.length);
+export default function computeFileSizesToDelete(inputLines) {
+    let path = ['root'];
+    let dirSizes = {};
+    let isList = false;
 
     inputLines.forEach(line => {
 
+        if (line.startsWith('$')) {
+            // it is a command
+            const parts = line.split(' ');
+            const cmd = parts[1];
+
+            if (cmd === 'ls') {
+                isList = true;
+            } else if (cmd === 'cd') {
+                isList = false;
+
+                const param = parts[2];
+                if (param === '/') {
+                    path = ['root'];
+                } else if (param === '..') {
+                    path.pop();
+                } else {
+                    path.push(param);
+                }
+            }
+        } else if (isList) {
+            // it is output of a list command
+            const [size, name] = line.split(' ');
+
+            if (size !== 'dir') {
+                for (let i = 0; i <= path.length; i++) {
+                    const curPath = path.slice(0, i).join('/');
+                    if (!(curPath in dirSizes)) {
+                        dirSizes[curPath] = 0;
+                    }
+                    dirSizes[curPath] += parseInt(size, 10);
+                }
+            }
+        }
     });
+
+    const totalSize = dirSizes['root'];
+    const spaceAvailable = 70000000 - totalSize;
+    const needed = 30000000 - spaceAvailable;
+
+    return Object.values(dirSizes).sort((a,b) => a - b).find(v => v > needed);
 }
