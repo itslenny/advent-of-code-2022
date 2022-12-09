39407ad8911c6071b9c3c68f90de7391f8df5e26
itslenny <lenny@itslennysfault.com>
2022-12-09 11:31:08 -0600

    Day 8

--- src/tasks/day-08-1.js
+++ src/tasks/day-08-1.js
@@ -1,7 +1,65 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 1', inputLines.length);
+export default function countVisibleTrees(inputLines) {
+    const rowCount = inputLines.length;
+    const colCount = inputLines[0].length;
 
-    inputLines.forEach(line => {
+    function getTreeHeight(row, col) {
+        return parseInt(inputLines[row].charAt(col), 10);
+    }
 
-    });    
+    function isTreeVisible(row, col) {
+        const targetHeight = getTreeHeight(row, col);
+
+        // up
+        let blocked = false;
+        for (let i = row - 1; i >= 0; i--) {
+            if (getTreeHeight(i, col) >= targetHeight) {
+                blocked = true;
+                break;
+            }
+        }
+        if (!blocked) {
+            return true;
+        }
+
+        // down
+        blocked = false;
+        for (let i = row + 1; i < rowCount; i++) {
+            if (getTreeHeight(i, col) >= targetHeight) {
+                blocked = true;
+                break;
+            }
+        }
+        if (!blocked) {
+            return true;
+        }
+
+        // left
+        blocked = false;
+        for (let i = col - 1; i >= 0; i--) {
+            if (getTreeHeight(row, i) >= targetHeight) {
+                blocked = true;
+                break;
+            }
+        }
+        if (!blocked) {
+            return true;
+        }
+
+        // right
+        blocked = false;
+        for (let i = col + 1; i < colCount; i++) {
+            if (getTreeHeight(row, i) >= targetHeight) {
+                blocked = true;
+                break;
+            }
+        }
+        if (!blocked) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    return inputLines.reduce((acc, row, rI) => acc + row.split('').reduce((rowAcc, v, cI) => rowAcc + isTreeVisible(rI, cI), 0), 0)
 }
--- src/tasks/day-08-2.js
+++ src/tasks/day-08-2.js
@@ -1,7 +1,55 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 2', inputLines.length);
+export default function computeHighestScenicScore(inputLines) {
+    const rowCount = inputLines.length;
+    const colCount = inputLines[0].length;
 
-    inputLines.forEach(line => {
+    function getTreeHeight(row, col) {
+        return parseInt(inputLines[row].charAt(col), 10);
+    }
 
-    });
+    function computeScenicScore(row, col) {
+        const targetHeight = getTreeHeight(row, col);
+
+        // up
+        let upScore = 0
+        for (let i = row - 1; i >= 0; i--) {
+            upScore++;
+            if (getTreeHeight(i, col) >= targetHeight) {
+                break;
+            }
+        }
+
+        // down
+        let downScore = 0;
+        for (let i = row + 1; i < rowCount; i++) {
+            downScore++;
+            if (getTreeHeight(i, col) >= targetHeight) {
+                break;
+            }
+        }
+
+        // left
+        let leftScore = 0;
+        for (let i = col - 1; i >= 0; i--) {
+            leftScore++;
+            if (getTreeHeight(row, i) >= targetHeight) {
+                break;
+            }
+        }
+
+        // right
+        let rightScore = 0;
+        for (let i = col + 1; i < colCount; i++) {
+            rightScore++;
+            if (getTreeHeight(row, i) >= targetHeight) {
+                break;
+            }
+        }
+
+        return leftScore * rightScore * upScore * downScore;
+
+    }
+
+    let scores = [];
+    inputLines.forEach((row, rI) => row.split('').forEach((v, cI) => scores.push(computeScenicScore(rI, cI))));
+    return scores.sort((a,b) => b - a)[0];
 }
