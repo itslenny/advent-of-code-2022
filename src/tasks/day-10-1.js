1d35c7c27d31ed3422f3582bb91f1c2733fc1853
itslenny <lenny@itslennysfault.com>
2022-12-11 14:05:14 -0600

    Day 10

--- src/tasks/day-10-1.js
+++ src/tasks/day-10-1.js
@@ -1,7 +1,27 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 1', inputLines.length);
+export default function countInterestingCycles(inputLines) {
+    const interestingCycles = new Set([20, 60, 100, 140, 180, 220]);
+
+    let xVal = 1;
+    let cycle = 0;
+
+    const vals = [];
+    const incrementCycle = () => {
+        cycle++;
+        if (interestingCycles.has(cycle)) {
+            vals.push(xVal * cycle);
+        }
+    }
 
     inputLines.forEach(line => {
+        if (line === 'noop') {
+            incrementCycle();
+        } else {
+            const [cmd, value] = line.split(' ');
+            incrementCycle();
+            incrementCycle();
+            xVal += parseInt(value, 10);
+        }
+    });
 
-    });    
+    return vals.reduce((acc, v) => acc + v, 0);
 }
--- src/tasks/day-10-2.js
+++ src/tasks/day-10-2.js
@@ -1,7 +1,32 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 2', inputLines.length);
+export default function renderScreenPictureCRT(inputLines) {
+    let xVal = 1;
+    let cycle = 0;
+
+    const vals = [];
+    const incrementCycle = () => {
+        const rowPos = [cycle % 40, (cycle + 1) % 40, (cycle - 1) % 40];
+        const val = rowPos.includes(xVal) ? '#' : '.';
+        vals.push(val);
+        cycle++;
+    }
 
     inputLines.forEach(line => {
-
+        if (line === 'noop') {
+            incrementCycle();
+        } else {
+            const [cmd, value] = line.split(' ');
+            incrementCycle();
+            incrementCycle();
+            xVal += parseInt(value, 10);
+        }
     });
+
+    let output = '';
+    let i = 0;
+    while (i < vals.length) {
+        output += vals.slice(i, i + 40).join('') + '\n';
+        i += 40;
+    }
+
+    return output;
 }
