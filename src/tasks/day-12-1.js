810cf91cc68f8bed8960961e31fafaef34782cf2
itslenny <lenny@itslennysfault.com>
2022-12-12 12:54:14 -0600

    Day 12

--- src/tasks/day-12-1.js
+++ src/tasks/day-12-1.js
@@ -1,7 +1,53 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 1', inputLines.length);
+export default function findShortestPath(inputLines) {
+    let startPos, endPos;
+    const heights = inputLines.map((line, y) => {
+        return line.split('').map((char, x) => {
+            if (char === 'S') {
+                startPos = { x, y };
+                return 'a'.charCodeAt(0);
+            } else if (char === 'E') {
+                endPos = { x, y };
+                return 'z'.charCodeAt(0);
+            }
+            return char.charCodeAt(0);
+        });
+    });
 
-    inputLines.forEach(line => {
+    let visited = heights.map((line) => line.map(() => false));
+    let shortestPaths = heights.map((line) => line.map(() => Infinity));
+    shortestPaths[endPos.y][endPos.x] = 0;
 
-    });    
+    let queue = [endPos];
+
+    while (queue.length > 0) {
+        let pos = queue.shift();
+        visited[pos.y][pos.x] = true;
+
+        let neighbors = [
+            { x: pos.x, y: pos.y - 1 },
+            { x: pos.x, y: pos.y + 1 },
+            { x: pos.x - 1, y: pos.y },
+            { x: pos.x + 1, y: pos.y },
+        ].filter((neighbour) => {
+            return heights[neighbour.y]?.[neighbour.x] !== undefined;
+        });
+
+        neighbors.forEach((neighbor) => {
+            let curHeight = heights[pos.y][pos.x];
+            let nextHeight = heights[neighbor.y][neighbor.x];
+            if (curHeight >= nextHeight - 1) {
+                let shortestDist = shortestPaths[neighbor.y][neighbor.x] + 1;
+                let currShortestDist = shortestPaths[pos.y][pos.x];
+                shortestPaths[pos.y][pos.x] = Math.min(currShortestDist, shortestDist);
+            }
+
+            if (!visited[neighbor.y][neighbor.x] && curHeight <= nextHeight + 1) {
+                queue.push(neighbor);
+                visited[neighbor.y][neighbor.x] = true;
+            }
+        });
+    }
+
+    return shortestPaths[startPos.y][startPos.x];
+
 }
--- src/tasks/day-12-2.js
+++ src/tasks/day-12-2.js
@@ -1,7 +1,57 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 2', inputLines.length);
-
-    inputLines.forEach(line => {
-
+export default function findShortestAPath(inputLines) {
+    let startPos, endPos;
+    let startOptions = [];
+    const heights = inputLines.map((line, y) => {
+        return line.split('').map((char, x) => {
+            if (char === 'S' || char === 'a') {
+                startOptions.push({ x, y });
+            }
+            
+            if (char === 'S' || char === 'a') {
+                startPos = { x, y };
+                return 'a'.charCodeAt(0);
+            } else if (char === 'E') {
+                endPos = { x, y };
+                return 'z'.charCodeAt(0);
+            }
+            return char.charCodeAt(0);
+        });
     });
+
+    let visited = heights.map((line) => line.map(() => false));
+    let shortestPaths = heights.map((line) => line.map(() => Infinity));
+    shortestPaths[endPos.y][endPos.x] = 0;
+
+    let queue = [endPos];
+
+    while (queue.length > 0) {
+        let pos = queue.shift();
+        visited[pos.y][pos.x] = true;
+
+        let neighbors = [
+            { x: pos.x, y: pos.y - 1 },
+            { x: pos.x, y: pos.y + 1 },
+            { x: pos.x - 1, y: pos.y },
+            { x: pos.x + 1, y: pos.y },
+        ].filter((neighbour) => {
+            return heights[neighbour.y]?.[neighbour.x] !== undefined;
+        });
+
+        neighbors.forEach((neighbor) => {
+            let curHeight = heights[pos.y][pos.x];
+            let nextHeight = heights[neighbor.y][neighbor.x];
+            if (curHeight >= nextHeight - 1) {
+                let shortestDist = shortestPaths[neighbor.y][neighbor.x] + 1;
+                let currShortestDist = shortestPaths[pos.y][pos.x];
+                shortestPaths[pos.y][pos.x] = Math.min(currShortestDist, shortestDist);
+            }
+
+            if (!visited[neighbor.y][neighbor.x] && curHeight <= nextHeight + 1) {
+                queue.push(neighbor);
+                visited[neighbor.y][neighbor.x] = true;
+            }
+        });
+    }
+
+    return startOptions.map(({ x, y }) => shortestPaths[y][x]).sort((a, b) => a - b)[0];
 }
