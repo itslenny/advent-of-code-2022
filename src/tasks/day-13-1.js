009aa683baacc8fb70d127d81a7766dafb8977d5
itslenny <lenny@itslennysfault.com>
2022-12-13 13:34:34 -0600

    Day 13

--- src/tasks/day-13-1.js
+++ src/tasks/day-13-1.js
@@ -1,7 +1,51 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 1', inputLines.length);
+function compareRightOrder(left, right) {
+    if (Array.isArray(right) && Array.isArray(left)) {
+        // both arrays
+        for (let i = 0; ; i++) {
+            if (i >= left.length && i >= right.length) {
+                return 0;
+            } else if (right.length > i && i >= left.length) {
+                return -1;
+            } else if (left.length > i && i >= right.length) {
+                return 1;
+            } else {
+                const result = compareRightOrder(left[i], right[i]);
+                if (result !== 0) {
+                    return result;
+                }
+            }
+        }
+    } else if (!Array.isArray(left) && Array.isArray(right)) {
+        // convert left to array
+        return compareRightOrder([left], right);
+    } else if (!Array.isArray(right) && Array.isArray(left)) {
+        // convert right to array
+        return compareRightOrder(left, [right]);
+    } else {
+        // both numbers
+        if (left === right) {
+            return 0;
+        }
+        return left < right ? -1 : 1;
+    }
+}
+
+export default function findRightPackets(inputLines) {
+
+    let packets = [];
+    let first = true;
 
     inputLines.forEach(line => {
+        if (line.length > 0) {
+            let parsed = JSON.parse(line);
+            if (first) {
+                packets.push({ left: parsed });
+            } else {
+                packets[packets.length - 1].right = parsed;
+            }
+            first = !first;
+        }
+    });
 
-    });    
+    return packets.reduce((acc, packet, i) => acc + (compareRightOrder(packet.left, packet.right) === -1 ? i + 1 : 0), 0);
 }
--- src/tasks/day-13-2.js
+++ src/tasks/day-13-2.js
@@ -1,7 +1,42 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 2', inputLines.length);
+function compareRightOrder(left, right) {
+    if (Array.isArray(right) && Array.isArray(left)) {
+        // both arrays
+        for (let i = 0; ; i++) {
+            if (i >= left.length && i >= right.length) {
+                return 0;
+            } else if (right.length > i && i >= left.length) {
+                return -1;
+            } else if (left.length > i && i >= right.length) {
+                return 1;
+            } else {
+                const result = compareRightOrder(left[i], right[i]);
+                if (result !== 0) {
+                    return result;
+                }
+            }
+        }
+    } else if (!Array.isArray(left) && Array.isArray(right)) {
+        // convert left to array
+        return compareRightOrder([left], right);
+    } else if (!Array.isArray(right) && Array.isArray(left)) {
+        // convert right to array
+        return compareRightOrder(left, [right]);
+    } else {
+        // both numbers
+        if (left === right) {
+            return 0;
+        }
+        return left < right ? -1 : 1;
+    }
+}
 
-    inputLines.forEach(line => {
+const DIVIDERS = ['[[2]]', '[[6]]'];
 
-    });
+export default function findSortedDividerPackets(inputLines) {
+    return inputLines
+        .concat(DIVIDERS)
+        .filter(v => v.length > 0)
+        .map(JSON.parse)
+        .sort(compareRightOrder)
+        .reduce((acc, v, i) => acc * (DIVIDERS.includes(JSON.stringify(v)) ? i + 1 : 1), 1)
 }
