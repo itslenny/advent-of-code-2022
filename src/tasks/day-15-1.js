b337a829028e39c8b009230298a43ccdb1eab40f
itslenny <lenny@itslennysfault.com>
2022-12-15 17:12:26 -0600

    Day 15

--- src/tasks/day-15-1.js
+++ src/tasks/day-15-1.js
@@ -1,7 +1,31 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 1', inputLines.length);
+export default function findMissingSpacesInTargetRow(inputLines) {
+    const isTest = process.argv[3] === 'test';
+    const targetRow = isTest ? 10 : 2000000;
+    console.log('isTest', isTest, 'targetRow', targetRow);
+
+    const targetRowCoords = new Set();
+    const targetRowBeacons = new Set();
 
     inputLines.forEach(line => {
+        const [sInput, bInput] = line.split('closest');
+        const [s, sX, sY] = sInput.match(/x=(-?\d+), y=(-?\d+)/).map(v => parseInt(v, 10));
+        const [b, bX, bY] = bInput.match(/x=(-?\d+), y=(-?\d+)/).map(v => parseInt(v, 10));
 
-    });    
+        if (bY === targetRow) {
+            targetRowBeacons.add(bX);
+        }
+
+        const dist = Math.abs(sX - bX) + Math.abs(sY - bY);
+        const startRow = sY - dist;
+        const endRow = sY + dist;
+
+        if (targetRow >= startRow && targetRow <= endRow) {
+            const width = dist - Math.abs(sY - targetRow);
+            for (let x = sX - width; x <= sX + width; x++) {
+                targetRowCoords.add(x);
+            }
+        }
+    });
+
+    return Array.from(targetRowCoords).filter(v => !targetRowBeacons.has(v)).length;
 }
--- src/tasks/day-15-2.js
+++ src/tasks/day-15-2.js
@@ -1,7 +1,35 @@
-export default function doStuff(inputLines) {
-    console.log('inputLines - 2', inputLines.length);
+export default function findBeacon(inputLines) {
+    const isTest = process.argv[3] === 'test';
+    const max = isTest ? 20 : 4000000;
 
-    inputLines.forEach(line => {
+    console.log('isTest', isTest, 'max', max);
 
+    const sensors = inputLines.map(line => {
+        const [sInput, bInput] = line.split('closest');
+        const [s, sX, sY] = sInput.match(/x=(-?\d+), y=(-?\d+)/).map(v => parseInt(v, 10));
+        const [b, bX, bY] = bInput.match(/x=(-?\d+), y=(-?\d+)/).map(v => parseInt(v, 10));
+        const range = Math.abs(sX - bX) + Math.abs(sY - bY);
+        return { sX, sY, range };
     });
+
+    for (let y = 0; y <= max; y++) {
+        let x = 0
+        for (; x <= max; x++) {
+
+            let sensorCanHitUs = false;
+            for (let { sX, sY, range } of sensors) {
+                const dist = Math.abs(sX - x) + Math.abs(sY - y);
+                if (dist <= range) {
+                    const widthThisRow = range - Math.abs(sY - y);
+                    x = sX + widthThisRow;
+                    sensorCanHitUs = true;
+                    break;
+                }
+            }
+            if (!sensorCanHitUs) {
+                console.log('x,y', x, y);
+                return 4000000 * x + y;
+            }
+        }
+    }
 }
